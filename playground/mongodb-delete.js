aad0eef0ca8819023c06d477c0e9ec769820a4be
Alan Astudillo <alanastudillo@hotmail.com>
2018-04-01 10:09:36 -0500

    Added delete

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,39 @@
+
+const {MongoClient, ObjectID} = require('mongodb');
+
+// Conectar a la base de datos
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (error, client) => {
+  if(error){
+    console.log('Unable to connect to database.');
+    return;
+  }
+
+  console.log('Connected succesfully.');
+
+  const db = client.db('TodoApp');
+
+  // Delete Many
+
+  // db.collection('Todos').deleteMany({text: 'Something to do'}).then( (result) => {
+  //   console.log(result);
+  // }, (error) => {
+  //   console.log('Error');
+  // });
+
+  // Delete One
+
+  // db.collection('Todos').deleteOne({text: 'Something to do'}).then( (result) => {
+  //   console.log(result);
+  // }, (error) => {
+  //   console.log('Error');
+  // });
+
+  db.collection('Todos').findOneAndDelete({completed: false}).then( (result) => {
+    console.log(result);
+  }, (error) => {
+    console.log('Error');
+  })
+
+  // Cerrar conexion
+  client.close();
+});
